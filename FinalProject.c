f1e93d697c297281170896f6e8019073b38a9b24
NavidFarahmand <NavidFarahmand75@gmail.com>
2017-06-05 06:42:26 +0430

    change all of content to what main.c was

--- FinalProject.c
+++ FinalProject.c
@@ -1,71 +1,47 @@
-#include<avr/io.h>
-#define F_CPU 1000000UL
-#include<util/delay.h>
-#include<avr/interrupt.h>
-#define sampleRate 2000
-void reset(){
-	//enable buzzer & ready led
-	DDRB=0b00000011;
-	PORTB=0b00000001;
-	//Analog 2 Digital Config
-	ADMUX=(1<<REFS0);                         // For Aref=AVcc;
-	ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); //Rrescalar div factor =128
-	//enable global interupt
-	//sei();
-}
-void ready()
+#include "constant.h"
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "usart.h"
+#include "adc.h"
+#include "led.h"
+
+int state=ACTIVE_NORMAL;
+int main (void)
 {
-	PORTB= PINB | 0b00000010;
-}
-void ringTheBuzzer(){
-	int i;
-	for ( i=0;i<400;i++){
-		PORTB=PINB ^ 0b0000001;
-		_delay_ms(50);
-		}
-		PORTB=PINB ^ 0b0000001;
-}
-void turnOnTheBuzzer(){
-	PORTB=PINB & 0b11111110;
-}
-void turnOffTheBuzzer(){
-	PORTB=PINB | 0b00000001;
-}
-uint16_t analogRead(uint8_t ch)
-{
-   ch=ch&0b00000111;
-   ADMUX|=ch;
-
-   ADCSRA|=(1<<ADSC);
-
-   while(!(ADCSRA & (1<<ADIF)));
-
-   ADCSRA|=(1<<ADIF);
-
-   return(ADC);
-}
-
-float getData(int channel){
-float result=0;
-int i=0;
-for(;i<sampleRate;i++){
-	result+=analogRead(channel);
-}
-result=result/sampleRate;
-return result;
-}
-int main(){
-	reset();
-	ready();
-	while(1)
-	{
-//		if(getData(0)>650)
-		turnOnTheBuzzer();
-//		ringTheBuzzer();
-//		else
-//		turnOffTheBuzzer();
-	//	_delay_ms(200);
-//		turnOffTheBuzzer();
-	}
+    int fire;
+    int reported=0;
+    serialInit(2400, 1, 1);
+    adcInit();
+    ledInit();
+    while(1)
+    {
+    fire = adcSmooth(0);
+        if(fire > FIRE_THRESHOLD)
+        {
+            switch(state)
+            {
+                case ACTIVE_NORMAL:
+	            reported=0;
+                    ledOn();
+                    break;
+                case ACTIVE_QUIET:
+		    if(reported==0)
+			    serialWriteString("Fire \r\n");
+		    reported=1;
+		    ledOff();
+                    break;
+                case DEACTIVE:
+		    reported=0;
+		    ledOff();
+                    break;
+            }
+        } else {
+            reported=0;
+            ledOff();
+        }
+    }
 }
 
